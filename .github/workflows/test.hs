d7e53459c2d11375d1d1ad1ae6d9fb9ff8dac22f
Chris Martin <ch.martin@gmail.com>
2021-05-26 17:54:15 -0600

    enable github ci

---
+++ .github/workflows/test.hs
@@ -0,0 +1,14 @@
+import System.Environment
+import System.Process
+
+main =
+  getEnv "ghc" >>= \ghc ->
+    case ghc of
+      "8.6.5" -> callProcess "cabal" ["test", "all"
+                  , "--constraint=criterion == 1.5.0.0"
+                  , "--constraint=hedgehog == 1.0"
+                  ]
+      "8.8.1" -> callProcess "cabal" ["test", "all"
+                  , "--constraint=criterion == 1.5.9.0"
+                  , "--constraint=hedgehog == 1.0.5"
+                  ]
