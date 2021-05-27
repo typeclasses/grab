d12eb88f9d973b2998b6a9e7eece1f8adf071de4
Chris Martin <ch.martin@gmail.com>
2021-05-26 18:20:54 -0600

    add more explicit CI bounds

--- .github/workflows/test.hs
+++ .github/workflows/test.hs
@@ -5,11 +5,15 @@ main =
   getEnv "ghc" >>= \ghc ->
     case ghc of
       "8.6.5" -> callProcess "cabal" ["test", "all"
+                  , "--constraint=containers == 0.6.0.1"
                   , "--constraint=criterion == 1.5.0.0"
                   , "--constraint=hedgehog == 1.0"
+                  , "--constraint=text == 1.2.3.0"
                   ]
       "8.8.1" -> callProcess "cabal" ["test", "all"]
       "8.10.4" -> callProcess "cabal" ["test", "all"
+                  , "--constraint=containers == 0.6.4.1"
                   , "--constraint=criterion == 1.5.9.0"
                   , "--constraint=hedgehog == 1.0.5"
+                  , "--constraint=text == 1.2.4.1"
                   ]
