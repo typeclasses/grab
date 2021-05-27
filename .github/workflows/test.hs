ebe24a38c227428ef1cd8e583a9f8dbfc3ed20d8
Chris Martin <ch.martin@gmail.com>
2021-05-26 18:47:27 -0600

    indicate support for ghc 9.0

--- .github/workflows/test.hs
+++ .github/workflows/test.hs
@@ -4,14 +4,15 @@ import System.Process
 main =
   getEnv "ghc" >>= \ghc ->
     case ghc of
-      "8.6.5" -> callProcess "cabal" ["test", "all"
+      "8.6.5"  -> callProcess "cabal" ["test", "all"
                   , "--constraint=containers == 0.6.0.1"
                   , "--constraint=criterion == 1.5.0.0"
                   , "--constraint=hedgehog == 1.0"
                   , "--constraint=text == 1.2.3.0"
                   ]
-      "8.8.1" -> callProcess "cabal" ["test", "all"]
-      "8.10.4" -> callProcess "cabal" ["test", "all"
+      "8.8.1"  -> callProcess "cabal" ["test", "all"]
+      "8.10.4" -> callProcess "cabal" ["test", "all"]
+      "9.0.1"  -> callProcess "cabal" ["test", "all"
                   , "--constraint=containers == 0.6.4.1"
                   , "--constraint=criterion == 1.5.9.0"
                   , "--constraint=hedgehog == 1.0.5"
